8a4920054fc08821d82bb99b0cfd4b1708740ab1
lemo <lemo@gamstudio.net>
2022-08-04 14:18:05 +0800

    update

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -18,41 +18,39 @@ export default class Chat extends Component {
 		return (
 			<div className="chat">
 				<div className="videos">
-					<video ref={this.user1} autoPlay playsInline></video>
-					<div>
-						<button
-							onClick={() => {
-								this.openVideo = true;
-								this.setting();
-							}}
-						>
-							open video
-						</button>
-						<button
-							onClick={() => {
-								this.openAudio = true;
-								this.setting();
-							}}
-						>
-							open audio
-						</button>
-					</div>
-					<video ref={this.user2} autoPlay playsInline></video>
+					<video
+						ref={this.user1}
+						disablePictureInPicture="true"
+						onDoubleClick={() => this.user1.current.requestFullscreen()}
+						autoPlay
+						playsInline
+					></video>
+					<video
+						ref={this.user2}
+						disablePictureInPicture="true"
+						onDoubleClick={() => this.user2.current.requestFullscreen()}
+						autoPlay
+						playsInline
+					></video>
 				</div>
 				<div className="users">
-					{this.state.users.map((e) => (
-						<div key={e.name}>
-							<div>{e.name}</div>
-							{this.name != e.name ? <button onClick={() => this.onCall(e)}>call</button> : null}
+					{this.state.users.map((user) => (
+						<div key={user.name}>
+							<div>{user.name}</div>
+							{this.name != user.name ? (
+								<>
+									<button onClick={() => this.onCall(user, "video")}>video</button>
+									<button onClick={() => this.onCall(user, "audio")}>audio</button>
+									<button onClick={() => this.onCall(user, "screen")}>screen</button>
+									<button onClick={() => this.onCall(user, "both")}>both</button>
+								</>
+							) : null}
 						</div>
 					))}
 				</div>
 			</div>
 		);
 	}
-
-	openVideo = false;
-	openAudio = false;
 
 	user1 = React.createRef();
 	user2 = React.createRef();
@@ -68,25 +66,27 @@ export default class Chat extends Component {
 
 	componentDidMount() {
 		ws.socket = new Socket({ addr: "wss://web-rtc-server.lemonyxk.com", heartBeatInterval: 1 });
-		ws.socket.before = () => {
-			ws.socket.Global = { name: this.name };
-		};
+		ws.socket.before = () => (ws.socket.Global = { name: this.name });
 
 		ws.socket.AddListener("/UserList", this.userList.bind(this));
 		ws.socket.AddListener("/CreateOffer", this.createOffer.bind(this));
 		ws.socket.AddListener("/CreateAnswer", this.createAnswer.bind(this));
 		ws.socket.AddListener("/Login", this.login.bind(this));
 		ws.socket.AddListener("/RequestAccount", this.requestAccount.bind(this));
+
 		ws.socket.Start(() => ws.socket.Emit("/login", { name: this.name }));
 	}
 
 	componentWillUnmount() {
 		ws.socket.Close();
+
 		ws.socket.RemoveListener("/UserList");
 		ws.socket.RemoveListener("/CreateOffer");
 		ws.socket.RemoveListener("/CreateAnswer");
 		ws.socket.RemoveListener("/Login");
 		ws.socket.RemoveListener("/RequestAccount");
+
+		this.reset();
 	}
 
 	localStream = null;
@@ -103,22 +103,15 @@ export default class Chat extends Component {
 	async requestAccount(e, data) {
 		servers.iceServers[0].credential = data.password;
 		servers.iceServers[0].username = data.account;
-		await this.initStream();
 	}
 
-	async initStream() {
-		await this.setting();
-	}
+	async createPeerConnection() {
+		this.reset();
 
-	async createPeerConnection(sdpType) {
 		this.peerConnection = new RTCPeerConnection(servers);
 
 		this.remoteStream = new MediaStream();
 		this.user2.current.srcObject = this.remoteStream;
-
-		this.localStream.getTracks().forEach((track) => {
-			this.peerConnection.addTrack(track, this.localStream);
-		});
 
 		this.peerConnection.ontrack = async (event) => {
 			event.streams[0].getTracks().forEach((track) => {
@@ -131,41 +124,15 @@ export default class Chat extends Component {
 		let sdp = this.peerConnection.localDescription;
 
 		this.peerConnection.onicecandidate = async (event) => {
-			if (event.candidate) {
-				sdp = this.peerConnection.localDescription;
-			}
+			if (event.candidate) sdp = this.peerConnection.localDescription;
 		};
 
 		return new Promise((r, j) => {
 			this.peerConnection.onicegatheringstatechange = async (event) => {
-				switch (this.peerConnection.iceGatheringState) {
-					case "new":
-						/* gathering is either just starting or has been reset */
-						break;
-					case "gathering":
-						/* gathering has begun or is ongoing */
-						break;
-					case "complete":
-						/* gathering has ended */
-						r(sdp);
-						break;
-				}
+				// new gathering complete
+				if (this.peerConnection.iceGatheringState == "complete") r(sdp);
 			};
 		});
-	}
-
-	async setting() {
-		if (!this.openVideo && !this.openAudio) {
-			return;
-		}
-
-		this.localStream = await navigator.mediaDevices.getUserMedia({ video: this.openVideo, audio: this.openAudio });
-		this.user1.current.srcObject = this.localStream;
-		if (this.peerConnection != null) {
-			this.localStream.getTracks().forEach((track) => {
-				this.peerConnection.addTrack(track, this.localStream);
-			});
-		}
 	}
 
 	userList(e, data) {
@@ -188,17 +155,38 @@ export default class Chat extends Component {
 
 	async onAddAnswer(data) {
 		let answer = JSON.parse(data.data);
-		if (!this.peerConnection.currentRemoteDescription) {
-			this.peerConnection.setRemoteDescription(answer);
+		this.peerConnection.setRemoteDescription(answer);
+		// if (!this.peerConnection.currentRemoteDescription) {
+		// 	this.peerConnection.setRemoteDescription(answer);
+		// }
+	}
+
+	getType(type) {
+		if (type == "video") return { video: true, audio: false };
+		if (type == "audio") return { video: false, audio: true };
+		if (type == "both") return { video: true, audio: true };
+		if (type == "screen") return { video: true, audio: true };
+	}
+
+	reset() {
+		if (this.peerConnection) this.peerConnection.close();
+		if (this.localStream) {
+			this.localStream.getTracks().forEach((track) => {
+				track.stop();
+			});
 		}
 	}
 
 	async onAnswer(data) {
-		if (!this.openVideo && !this.openAudio) {
-			return alert("At least one of audio and video must be requested");
-		}
-
 		await this.createPeerConnection();
+
+		if (data.type != "screen") {
+			this.localStream = await navigator.mediaDevices.getUserMedia(this.getType(data.type));
+			this.user1.current.srcObject = this.localStream;
+			this.localStream.getTracks().forEach((track) => {
+				this.peerConnection.addTrack(track, this.localStream);
+			});
+		}
 
 		let offer = JSON.parse(data.data);
 		await this.peerConnection.setRemoteDescription(offer);
@@ -211,18 +199,24 @@ export default class Chat extends Component {
 		ws.socket.Emit("/createAnswer", { from: data.to, to: data.from, data: JSON.stringify(this.answer) });
 	}
 
-	async onCall(user) {
-		if (!this.openVideo && !this.openAudio) {
-			return alert("At least one of audio and video must be requested");
+	async onCall(user, type) {
+		await this.createPeerConnection();
+
+		if (type == "screen") {
+			this.localStream = await navigator.mediaDevices.getDisplayMedia(this.getType(type));
+		} else {
+			this.localStream = await navigator.mediaDevices.getUserMedia(this.getType(type));
 		}
 
-		await this.createPeerConnection();
+		this.user1.current.srcObject = this.localStream;
+		this.localStream.getTracks().forEach((track) => {
+			this.peerConnection.addTrack(track, this.localStream);
+		});
 
 		let offer = await this.peerConnection.createOffer();
 		await this.peerConnection.setLocalDescription(offer);
 
 		this.offer = await this.wait();
-
-		ws.socket.Emit("/createOffer", { from: this.name, to: user.name, data: JSON.stringify(this.offer) });
+		ws.socket.Emit("/createOffer", { type: type, from: this.name, to: user.name, data: JSON.stringify(this.offer) });
 	}
 }
